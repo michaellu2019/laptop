79eb3084fdeb54f1c2ca47e580dcc58dcbaf591d
Michael Lu <michaellu2019@gmail.com>
2022-07-31 23:08:06 -0400

    Removed unecessary folders.

--- build/arm_link_assembly_urdf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mlu/Documents/Programming/ROS/laptop_ws/src/arm_link_assembly_urdf
-# Build directory: /home/mlu/Documents/Programming/ROS/laptop_ws/build/arm_link_assembly_urdf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/mlu/Documents/Programming/ROS/laptop_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/mlu/Documents/Programming/ROS/laptop_ws/devel;/opt/ros/noetic'.split(';') if '/home/mlu/Documents/Programming/ROS/laptop_ws/devel;/opt/ros/noetic' != '' else []
